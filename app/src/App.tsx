6babba296ce79cbeacb9314c5eebcdf5c232d11b
Oliver Cadman <o.cadman@live.co.uk>
2023-06-16 12:40:33 +0100

    Configured base scss variables and utilities

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="App p-1 pl-md-10 pr-md-10">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
