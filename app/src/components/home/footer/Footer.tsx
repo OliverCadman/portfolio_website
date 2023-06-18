d442e2ce35b2bf8bf05f2bfed1e0143a556e8aae
Oliver Cadman <o.cadman@live.co.uk>
2023-06-19 00:00:24 +0100

    Configured emailJS

--- app/src/components/home/footer/Footer.tsx
+++ app/src/components/home/footer/Footer.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { BaseSyntheticEvent, useRef } from "react";
 import Links from "../links/Links";
+import emailjs from "@emailjs/browser";
 
 const Footer = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = (e: BaseSyntheticEvent) => {
+    e.preventDefault();
+    const formData = e.target;
+    const name = formData.name.value;
+    const email = formData.email.value;
+    const message = formData.message.value;
+
+    if (!name) {
+      console.log("Name invalid.");
+    }
+
+    if (!email || email.indexOf("@") === -1) {
+      console.log("Email invalid");
+    }
+
+    if (!message) {
+      console.log("message invalid");
+    }
+
+    const serviceID = process.env.EMAILJS_SERVICE_ID;
+    const templateID = process.env.EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.EMAILJS_PUBLIC_KEY;
+
+    emailjs
+      .sendForm(serviceID, templateID, formRef.current, publicKey)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="contact-section__container">
       <div className="contact-section__wrapper">
@@ -13,18 +49,30 @@ const Footer = () => {
           </p>
         </div>
         <div className="contact-section__form">
-          <form action="#">
+          <form action="#" onSubmit={handleSubmit} ref={formRef}>
             <div className="form-group">
               <label htmlFor="name" className="sr-only">
                 Name
               </label>
-              <input type="text" name="name" id="name" placeholder="NAME" />
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="NAME"
+                formNoValidate={true}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="email" className="sr-only">
                 Email
               </label>
-              <input type="email" name="email" id="email" placeholder="EMAIL" />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="EMAIL"
+                formNoValidate={true}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="message" className="sr-only">
