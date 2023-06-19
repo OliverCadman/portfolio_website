dfb647552cffb2ace6c700d6d12500432bf7a102
Oliver Cadman <o.cadman@live.co.uk>
2023-06-19 11:10:40 +0100

    Added SVGs to home hero

--- app/src/components/home/hero/Hero.tsx
+++ app/src/components/home/hero/Hero.tsx
@@ -1,39 +1,45 @@
 import heroAvatarDesktop from "../../../assets/images/image-profile-desktop.webp";
 import heroAvatarTablet from "../../../assets/images/image-profile-tablet.webp";
 import heroAvatarMobile from "../../../assets/images/image-profile-mobile.webp";
+import { ReactComponent as PatternRings } from "../../../assets/images/pattern-rings.svg";
+import { ReactComponent as PatternCircle } from "../../../assets/images/pattern-circle.svg";
 
 const Hero = () => {
   return (
-    <section className="section hero__content">
-      <div className="hero__avatar-container">
-        <picture className="hero__img">
-          <source media="(min-width: 768px)" srcSet={heroAvatarTablet} />
-          <source media="(min-width: 1220px)" srcSet={heroAvatarDesktop} />
-          <img
-            src={heroAvatarMobile}
-            alt="Avatar of website owner."
-            className="hero__img"
-          />
-        </picture>
+    <section className="section hero-section">
+      <div className="hero__content">
+        <div className="hero__avatar-container">
+          <PatternCircle className="hero-circle" />
+          <picture className="hero__img">
+            <source media="(min-width: 768px)" srcSet={heroAvatarTablet} />
+            <source media="(min-width: 1220px)" srcSet={heroAvatarDesktop} />
+            <img
+              src={heroAvatarMobile}
+              alt="Avatar of website owner."
+              className="hero__img"
+            />
+          </picture>
+        </div>
+        <div className="hero__callout">
+          <article>
+            <h2>
+              Nice to meet you! I'm{" "}
+              <span className="underlined">Oli Cadman.</span>
+            </h2>
+            <p>
+              Based in London UK, I'm a Full Stack Developer, passionate about
+              building accessible web apps and data transformation pipelines.
+            </p>
+            <div className="hero__contact">
+              <a href="#" className="offset-colored-underline">
+                Contact Me
+              </a>
+            </div>
+          </article>
+        </div>
+        <hr />
       </div>
-      <div className="hero__callout">
-        <article>
-          <h2>
-            Nice to meet you! I'm{" "}
-            <span className="underlined">Oli Cadman.</span>
-          </h2>
-          <p>
-            Based in London UK, I'm a Full Stack Developer, passionate about
-            building accessible web apps and data transformation pipelines.
-          </p>
-          <div className="hero__contact">
-            <a href="#" className="offset-colored-underline">
-              Contact Me
-            </a>
-          </div>
-        </article>
-      </div>
-      <hr />
+      <PatternRings className="hero-rings" />
     </section>
   );
 };
